94374d4246f7e05a019a88bbbe2de5e1eaae4d2d
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-06-29 22:07:08 +0500

    Activity done

--- Lessonnumber3.py
+++ Lessonnumber3.py
@@ -1,1 +1,13 @@
-print("my name is muaaz")
+tree1 = 22
+tree2 = 24
+tree3 = 26
+tree4 = 28 
+tree5 = 30
+
+sum = tree1 + tree2 + tree3 + tree4 + tree5
+
+print(f"The sum of all trees are {sum}")
+
+average = sum/5
+
+print(f"The average of the trees are {average}")

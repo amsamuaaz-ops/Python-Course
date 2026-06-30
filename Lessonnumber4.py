accad2855df7c43bc83f3d1aabc47acd0c9ec997
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-06-30 21:31:27 +0500

    lesson 4 activity 1

---
+++ Lessonnumber4.py
@@ -0,0 +1,14 @@
+num = 4
+if(num > 3):
+    print("4 is greater then 3")
+
+
+
+num1 = -2
+if(num1 > 0):
+    print("Yes it is greater then 0")
+
+else: 
+    print("No its not greater then 0")
+
+  

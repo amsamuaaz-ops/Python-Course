74ee90fee058ade788d649392eb043e7b0729e26
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-02 21:36:09 +0500

    activity1

---
+++ lesson4activity1.py
@@ -0,0 +1,19 @@
+v = 2
+w = 5
+x = 9
+y =7
+z = w+v**x*y
+
+print(f"The sum of v w x and y is {z}")
+
+
+
+
+
+name = "Muaaz"
+age = 0
+
+if (name == "Muaaz" or name == "Jhon") and age >= 2:
+    print("Welcome")
+else: 
+    print("Goodbye")

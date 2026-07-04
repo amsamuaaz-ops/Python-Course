fa11c81f352f8d7a10a8c0a5e1d6c777c57c6b97
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-04 12:56:26 +0500

    hw3

---
+++ hw3.py
@@ -0,0 +1,8 @@
+print("Rohan wants to wear light cloths")
+print("but he dont want to be feel cold too ")
+print("what he can do")
+temp = int(input("What is the Temp outside"))
+if temp  >15 :
+    print("Rohan could wear light cloths because it temp ")
+else:
+    print("You should wait for the time it gets 15 degrre")

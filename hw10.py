27b61cadc36751d44b3fe7732a0988ebba421f34
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-09 19:00:08 +0500

    project2

---
+++ hw10.py
@@ -0,0 +1,7 @@
+age = int(input("Enter you age"))
+print("Minimum age to enter exam hall is 10 - 20")
+
+if age >=10:
+    print(f"Your age is {age} So you can enter")
+    if age <=10:
+        print("You cannot enter")

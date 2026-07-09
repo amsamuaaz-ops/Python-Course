05e6985637ecbd97358d8eb599a3cac2243d01b5
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-09 21:36:51 +0500

    project1

---
+++ activity1lesson10.py
@@ -0,0 +1,9 @@
+print("Right Angle Triangle\n")
+
+n = int(input(" Enter your integer"))
+
+for i in range(n):
+
+    for j in range(i+1):
+     print("* ",end= "")   
+    print()
---
+++ project2.py
@@ -0,0 +1,18 @@
+team1 = 20
+team2 = 40
+team3 = 60
+team4 = 65
+team5 = 85
+
+average = team1 + team2 + team3 + team4 + team5/5
+
+star_per_point = 2
+total_points = team1 + team2 + team3 + team4 + team5
+totalstarpoints = total_points*star_per_point
+
+box = 25
+store_points = totalstarpoints//box
+left_overstars = totalstarpoints % box
+
+lastweek = 270
+

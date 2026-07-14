5a6ff9ceb413dde07cc5cc939428b26257ddb750
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-14 21:26:15 +0500

    activity1tuples

---
+++ activity1tuples.py
@@ -0,0 +1,21 @@
+tuplex = (1,2,3,4,"muaaz","ahmed",2.14)
+print(tuplex)
+
+tuplex = (6,8,7)
+print(tuplex)
+
+tuple = tuplex + tuplex
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (1,2,3,4,5,6,7,8,3,4,9,10,3,9,2,1,10,9,9,10)
+count = tuple1.count(9)
+print(count)
+
+tuplex = (1,2,3,4,5,6,7,8,9,10)
+slicing = tuplex[3:4]
+print(slicing)
+slicing = tuplex[:6]
+print(slicing)
+
+

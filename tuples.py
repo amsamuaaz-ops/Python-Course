c131ad0297924b0b10211bcdf3bdfcd943749d69
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-07-16 17:43:36 +0500

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+tuple1 = (4,3,2,2,-1,18)
+tuple2 = (2,4,8,8,3,2,9)
+
+product1 = tuple1[0]* tuple1[1]* tuple1[2]* tuple1[3]* tuple1[4]* tuple1[5]
+
+print("The result for tuple2 is",product1)
+
+product2 = tuple2[0]* tuple2[1]* tuple2[2]* tuple2[3]* tuple2[4]* tuple2[5]
+
+print("The result for tuple2 is",product2)

e8717bbca088eae6a09cc66a83151ef8b8a53105
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-08-23 21:41:49 +0500

    ai

---
+++ ai.py
@@ -0,0 +1,20 @@
+print("Hi am a chatbot what is your name")
+
+name = input()
+
+print(f"Nice to meet you{name}")
+
+print("how is your mood today?")
+
+mood = input().lower()
+
+if mood == "good":
+    print("Glad to hear u have a good mood")
+
+elif mood == "bad":
+    print("feeling bad for you")
+
+else:
+    print("ITs really worst feeling having this kinda moods")
+
+print("nice to meet you ")

5cc5159cc62a8edd6220034fb95c676788b1aa8d
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-08-29 15:01:32 +0500

    hmi

---
+++ chatbothw.py
@@ -0,0 +1,13 @@
+from colorama import Fore,init
+init(autoreset=True)
+
+print(Fore.LIGHTMAGENTA_EX+"Hi I am A simple chatbot")
+print(Fore.GREEN + "\nWhat Is Your Name")
+name = input(Fore.RED),
+
+print(Fore.GREEN + "Nice to meet You",name)
+print(Fore.GREEN + " \n""Where Do You live")
+live = input (Fore.RED)
+print(Fore.GREEN + f"You live in a nice area {name} Btw Nice to meet you")
+
+print(Fore.YELLOW + "=====BYE=====")

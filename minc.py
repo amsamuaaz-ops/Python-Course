c8e8fae4c91bab5b6d37f02b66336e6902c1de3f
amsamuaaz-ops <amsamuaaz@gmail.com>
2026-09-02 19:24:03 +0500

    minc

--- minc.py
+++ minc.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras import models, layers
 
+
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 x_train, x_test = x_train / 255.0, x_test / 255.0
